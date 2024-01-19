fde08a62feefc9ceda82e5221bf72d49a2869dd8
Louai-Alaranji <alaranjileo99@gmail.com>
2024-01-19 19:48:14 +0100

    delete btn and date added
    now we can delete an item and add a date when we want beside the item.
    used Closure which is a method to keep track of the current index attached to the button.
    it is great in asynchronos operations.

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 const itemName = document.querySelector(".js-text-input");
 const addBtn = document.querySelector(".js-add");
 const itemsAddeddiv = document.querySelector(".js-todo-items");
-
+const todoDate = document.querySelector(".js-date");
 var todoList = [];
 
-function buttonClicked(){
-    todoList.push(itemName.value);
-    itemName.value = "";    // clear the field after item is added
-    itemsAddeddiv.innerHTML = "";
+function buttonClicked() {
+    todoList.push({ name: itemName.value, date: todoDate.value });
 
-    for(var i = 0; i<todoList.length; ++i){
-        var newPara = document.createElement("p");
-        newPara.textContent = todoList[i];
+    itemName.value = ""; // clear the field after the item is added
+    todoDate.value = "";
 
-        itemsAddeddiv.appendChild(newPara);
-    }
-    
+    renderTodoList();
 }
 
-/*
-another way is
-    
-    let todoListHtml = "";
-    for(var i = 0; i<todoList.length; ++i){
-        const todo = todoList[i];
-        const html = "<p>${todo}</p>";
+function renderTodoList() {
+    itemsAddeddiv.innerHTML = "";
 
-        todoListHtml += html;
+    for (var i = 0; i < todoList.length; ++i) {
+        var newPara = document.createElement("p");
+        var newBtn = document.createElement("button");
+        newBtn.innerText = "Delete";
+
+        // Use a closure to capture the current value of i
+        newBtn.onclick = (function (index) {
+            return function () {
+                deleteItem(index);
+            };
+        })(i);
+
+        newPara.textContent = todoList[i].name + " " + todoList[i].date;
+
+        newPara.appendChild(newBtn);
+        itemsAddeddiv.appendChild(newPara);
     }
+}
 
-    document.query(".js-todo-items").innerHTML = todoListHtml;
+function deleteItem(index) {
+    // Remove the item at the specified index from the todoList
+    todoList.splice(index, 1);
 
-*/ 
+    // Refresh the displayed items after deletion
+    renderTodoList();
+}
